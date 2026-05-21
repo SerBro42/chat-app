860ddfaaf4057281be440b90c1c3f255935761ac
Sergey Brovko <serbro7@gmail.com>
2026-05-21 15:08:48 +0200

    FEAT: installed sockjs and stompjs libraries. polyfills.ts created for global variables definition

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-chat',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
-export class Chat {
+export class Chat implements OnInit {
 
+  private client!: Client;
+  constructor() { }
+
+  ngOnInit() {
+    this.client = new Client();
+    this.client.webSocketFactory = ()=>{
+      return new SockJS("http://localhost:8080/chat-websocket");
+    }
+
+    this.client.onConnect = (frame) => {
+      console.log('Connectados: ' + this.client.connected + ' : ' + frame);
+    }
+
+    this.client.activate();
+  }
 }
---
+++ src/polyfills.ts
@@ -0,0 +1,4 @@
+import 'zone.js';  // Included with Angular CLI.
+
+// Polyfill for global variable in Angular 15 and later
+(window as any).global = window;
