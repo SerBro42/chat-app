a8e3f4404bedc44134ffcdf0965eaf865dad28bf
Sergey Brovko <serbro7@gmail.com>
2026-05-22 14:40:38 +0200

    FEAT: implemented sending and receiving messages

--- src/app/chat/chat.ts
+++ src/app/chat/chat.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
+import { Mensaje } from './models/mensaje';
+import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [
+    FormsModule,
+    DatePipe],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
@@ -12,6 +17,9 @@ export class Chat implements OnInit {
 
   private client!: Client;
   conectado: boolean = false;
+
+  mensaje: Mensaje = new Mensaje();
+  mensajes: Mensaje[] = [];
 
   constructor() { }
 
@@ -24,6 +32,15 @@ export class Chat implements OnInit {
     this.client.onConnect = (frame) => {
       console.log('Connectados: ' + this.client.connected + ' : ' + frame);
       this.conectado = true;
+
+      //We subscribe to the message broker, and then send a message to the server, which will be broadcasted to all clients subscribed to the same topic
+      this.client.subscribe('/chat/mensaje', (e) => { 
+        let mensaje: Mensaje = JSON.parse(e.body) as Mensaje;
+        mensaje.fecha = new Date(mensaje.fecha);
+        this.mensajes.push(mensaje);
+        console.log(mensaje);
+      });
+
     }
 
     this.client.onDisconnect = (frame) => {
@@ -39,5 +56,14 @@ export class Chat implements OnInit {
 
   desconectar(): void {
     this.client.deactivate();
+  }
+
+  enviarMensaje(): void {
+    this.client.publish({
+      destination: '/app/mensaje',
+      body: JSON.stringify(this.mensaje)
+    });
+
+    this.mensaje.texto = '';
   }
 }
